3d2e6baeff641fbd1e86e8790879bafee800525b
anvitared12 <anvitasreddy@gmail.com>
2025-07-30 21:32:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.shoppinglistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.shoppinglistapp.ui.theme.ShoppingListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShoppingListAppTheme {
+                AddItem()
+            }
+        }
+    }
+}
+
